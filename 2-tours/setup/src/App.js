12058ba31c22a3d7d451bf243c3c9f30b3a26a1e
shema derrick <shemakderrick@gmail.com>
2020-12-06 16:36:54 +0200

    first commit

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,7 +5,50 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,setLoading]=useState(true);
+  const [tours, setTours]=useState([]);
+
+  const removeTour=(id)=>{
+  const newtours = tours.filter((tour)=>tour.id!==id);
+  setTours(newtours);
+
+  }
+  const fetchTours = async()=>{
+try {
+  const response=await fetch(url);
+  const tours= await response.json();
+  setLoading(false);
+  setTours(tours);
+} catch (error) {
+  setLoading(false)
+  console.log(error);
+}
+
+} 
+
+useEffect(()=>{
+  fetchTours()
+},[])
+
+if(loading){
+  return<main>
+    <Loading/>
+  </main>
+}
+
+if(tours.length===0){
+  return <main>
+
+  <div className="title">
+<h4>No tours left</h4>
+    <button className="btn" onClick={fetchTours}> Refresh</button>
+  </div>
+
+  </main>
+}
+
+  return <main><Tours tours={tours} removeTour={removeTour}/></main>
+   
 }
 
 export default App
